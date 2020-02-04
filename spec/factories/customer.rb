1080bcaa12fa18d0cd7bd779ca577d026ff8f9d1
Adelino Masioli <adelinojunior@tz3.com.br>
2020-02-04 21:05:23 +0000

    Factory FactoryBot

---
+++ spec/factories/customer.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :customer do
+    name "Adelino Masioli"
+    email "adelinomasioli@gmail.com"
+  end
+ end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   
-  fixtures :customers
+  #fixtures :customers
 
+  #it 'Create a Customer' do
+    #customer = customers(:adelino)
+    #expect(customer.name).to eq("Adelino Masioli")
+  #end
   it 'Create a Customer' do
-    customer = customers(:adelino)
+    customer = create(:customer)
     expect(customer.name).to eq("Adelino Masioli")
   end
 end
