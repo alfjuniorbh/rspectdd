0c316cdb1ed92291b19d9234cc797795298e672a
Adelino Masioli <adelinojunior@tz3.com.br>
2020-02-05 18:33:14 +0000

    Create list

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  it 'New order' do
+  it 'Create order' do
     order = create(:order)
     expect(order.customer).to be_kind_of(Customer)
+  end
+
+  it 'Create order list' do
+    order = create_list(:order, 3, description: "Description Test")
+    expect(order.count).to eq(3)
+    puts order.inspect
   end
 end
