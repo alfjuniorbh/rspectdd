a5676510bbd333e02f00ede0709d3625f0629370
Adelino Masioli <adelinojunior@tz3.com.br>
2020-02-05 16:50:36 +0000

    Factory Herança

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -1,6 +1,17 @@
 FactoryBot.define do
   factory :customer do
-    name "Sr. #{Faker::Name.name}"
-    email Faker::Internet.email
+    name { "Sr. #{Faker::Name.name}" }
+    email { Faker::Internet.email }
+
+    factory :customer_vip do
+      vip { true }
+      days_to_pay { 30 }
+    end
+
+    factory :customer_default do
+      vip { false }
+      days_to_pay { 20 }
+    end
+
   end
  end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   
-  #fixtures :customers
+  fixtures :customers
 
-  #it 'Create a Customer' do
-    #customer = customers(:adelino)
-    #expect(customer.name).to eq("Adelino Masioli")
-  #end
   it 'Create a Customer' do
-    customer = create(:customer)
-    expect(customer.name).to start_with("Sr. ")
+    customer = customers(:adelino)
+    expect(customer.name).to eq("Adelino Masioli")
+  end
+  
+  it 'Create a Customer' do
+    customer = create(:customer_vip)
+    expect(customer.vip).to eq(true)
   end
 end
