ae204e8856f9547ecd0feb84319d48dcf20a2ac1
Adelino Masioli <alfjuniorbh.web@gmail.com>
2020-02-06 19:35:04 +0000

    VCR new episodes

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -17,8 +17,20 @@ describe 'HTTParty' do
     end
   end
 
-  it 'VCR Only', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
+  it 'VCR Only', :vcr do
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
+  end
+
+  it 'VCR body match', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/3')
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
+  end
+
+  it 'VCR new cassette', vcr: { cassette_name: 'jsonplaceholder/posts', :record => :new_episodes } do
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/4')
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
   end
