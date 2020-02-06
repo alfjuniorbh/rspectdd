77f023feeecfbd16626b8e939d6287c07e32462a
Adelino Masioli <alfjuniorbh.web@gmail.com>
2020-02-06 19:44:07 +0000

    Time Helper

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -30,4 +30,13 @@ RSpec.describe Customer, type: :model do
     expect(customer.gender).to eq('M')
   end
 
+  it 'Travel to' do
+    travel_to Time.zone.local(2004, 11, 24, 01, 04, 44) do
+      @customer = create(:customer_vip)
+    end
+    puts @customer.created_at
+    puts Time.now
+    expect(@customer.created_at).to eq(Time.new(2004, 11, 24, 01, 04, 44))
+  end
+
 end
