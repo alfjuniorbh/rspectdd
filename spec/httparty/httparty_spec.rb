4516aec4ffd95ca635cce97cbb363e9c89df42c1
Adelino Masioli <alfjuniorbh.web@gmail.com>
2020-02-06 19:15:18 +0000

    VCR

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -8,4 +8,12 @@ describe 'HTTParty' do
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
   end
+
+  it 'VCR' do
+    VCR.use_cassette("jsonplaceholder/posts") do
+      response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+      content_type = response.headers['content-type']
+      expect(content_type).to match(/application\/json/)
+    end
+  end
 end
