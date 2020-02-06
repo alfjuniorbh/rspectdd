ea1aba581bec21337677c7c9c703a7bcee7342ea
Adelino Masioli <alfjuniorbh.web@gmail.com>
2020-02-06 19:54:17 +0000

    Order random

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,6 +25,15 @@ end
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  #order random
+  config.order = "random"
+
+  #FactoryBot lint
+  config.before(:suite) do
+    FactoryBot.lint
+  end
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
