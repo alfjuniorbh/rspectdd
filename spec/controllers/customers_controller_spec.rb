80e652d5ce83e32e165c8a80f6bd26ab732ee160
Adelino Masioli <alfjuniorbh.web@gmail.com>
2020-02-07 20:34:53 +0000

    Test customer params

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -23,23 +23,30 @@ RSpec.describe CustomersController, type: :controller do
   end
 
   context 'Logged' do
+    before do
+      @member = create(:member)
+      @customer = create(:customer)
+    end
+
+    it 'valid attributes' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      expect {
+        post :create, params: { customer: customer_params }
+      }.to change(Customer, :count).by(1)
+    end
+
     it 'do login and return user' do
-      member = create(:member)
-      customer = create(:customer)
+      sign_in @member
   
-      sign_in member
-  
-      get :show, params: { id: customer.id }
+      get :show, params: { id: @customer.id }
       expect(response).to have_http_status(200)
     end
 
     it 'do login and show template' do
-      member = create(:member)
-      customer = create(:customer)
+      sign_in @member
   
-      sign_in member
-  
-      get :show, params: { id: customer.id }
+      get :show, params: { id: @customer.id }
       expect(response).to render_template(:show)
     end
   end
