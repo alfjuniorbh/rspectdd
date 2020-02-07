ed60369b66f90e6b090831593297bd930e317d6f
Adelino Masioli <alfjuniorbh.web@gmail.com>
2020-02-07 18:59:08 +0000

    Config Devise

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -1,15 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe CustomersController, type: :controller do
-
-  it "responds successfully" do
+  it 'responds successfully' do
     get :index
     expect(response).to be_success
-   end
-
-  it "returns a 200 response" do
-    get :index
-    expect(response).to have_http_status "200"
   end
 
+  it 'returns a 200 response' do
+    get :index
+    expect(response).to have_http_status '200'
+  end
 end
