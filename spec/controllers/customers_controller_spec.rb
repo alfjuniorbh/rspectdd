bb4bb0fdebeba0e42a6d3ffc3572162202582871
Adelino Masioli <alfjuniorbh.web@gmail.com>
2020-02-07 20:22:54 +0000

    Test login

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -3,13 +3,34 @@
 require 'rails_helper'
 
 RSpec.describe CustomersController, type: :controller do
-  it 'responds successfully' do
-    get :index
-    expect(response).to be_success
+
+  context 'Not Guest' do
+    it 'responds successfully' do
+      get :index
+      expect(response).to be_success
+    end
+
+    it 'returns status 200 response' do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+  
+    it 'returns status 302 response show' do
+      customer = create(:customer)
+      get :show, params: { id: customer.id }
+      expect(response).to have_http_status(302)
+    end
   end
 
-  it 'returns a 200 response' do
-    get :index
-    expect(response).to have_http_status '200'
+  context 'Logged' do
+    it 'do login and return user' do
+      member = create(:member)
+      customer = create(:customer)
+  
+      sign_in member
+  
+      get :show, params: { id: customer.id }
+      expect(response).to have_http_status(200)
+    end
   end
 end
