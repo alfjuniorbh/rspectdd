9e57d5d3c9fff1e5d9269de4aec32b56271eab2a
Adelino Masioli <adelinojunior@tz3.com.br>
2020-02-20 21:21:40 +0000

    Verify if exist Welcome link

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 feature "Welcome", type: :feature do
   scenario 'Show welcome message' do
-   visit('/')
+   visit(root_path)
    expect(page).to have_content('Welcome')
+  end
+
+  scenario 'Verify link Create new Customer' do
+    visit(root_path)
+    expect(find('ul li')).to have_link('Create new Customer')
   end
 end
